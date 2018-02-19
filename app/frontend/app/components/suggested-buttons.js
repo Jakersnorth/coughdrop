ce1cc7e1865040bbc66d527e600ca8b3834213f5
John Akers <johnfakersnorth@gmail.com>
2018-02-19 11:18:51 -0800

    Adds suggested button region on editing screen that can be toggled to be visible or not. Needs to be populated with button placeholders and add to board functionality

---
+++ app/frontend/app/components/suggested-buttons.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+import InboundActions from '../../ember-component-inbound-actions/inbound-actions';
+
+export default Ember.Component.extend(InboundActions, {
+	slideoutService: Ember.inject.service('slideout-service'),
+	init:function() {
+		this._super();
+		this.set('slideout', null);
+	},
+
+	didInsertElement: function() {
+		this._super();
+
+		var newSlideout = new window.Slideout({
+			'panel': Ember.$('#slideout-main-panel')[0],
+			'menu': Ember.$('#menu')[0],
+			'padding': 256,
+			'tolerance': 70
+		});
+
+		this.set('slideout', newSlideout);
+
+		Ember.$('#menu').css('padding-top', '70px');
+	},
+
+	toggleSlideout: function() {
+		this.get('slideout').toggle();
+	},
+
+	subscribeToService: Ember.on('init', function() {
+		this.get('slideoutService').on('toggleSlideout', this, this.toggleSlideout);
+	}),
+	unsubscribeToService: Ember.on('willDestroyElement', function() {
+		this.get('slideoutService').off('toggleSlideout', this, this.toggleSlideout);
+	})
+});
