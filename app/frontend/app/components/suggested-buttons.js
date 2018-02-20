90bd5f8edc1679685d90d96ba91b0c7f0544206b
John Akers <johnfakersnorth@gmail.com>
2018-02-19 21:47:36 -0800

    connects sidebar with main board to add suggested buttons to the board

--- app/frontend/app/components/suggested-buttons.js
+++ app/frontend/app/components/suggested-buttons.js
@@ -1,13 +1,25 @@
 import Ember from 'ember';
 import InboundActions from '../../ember-component-inbound-actions/inbound-actions';
+import editManager from '../utils/edit_manager';
 
 export default Ember.Component.extend(InboundActions, {
 	slideoutService: Ember.inject.service('slideout-service'),
+	//suggestionController: Ember.inject.controller('button-suggestions'),
 	init:function() {
 		this._super();
 		this.set('slideout', null);
 	},
-
+	check_availability: function() {
+		var rows = editManager.controller.get('ordered_buttons');
+		var empty = 0;
+		rows.forEach(function(row) {
+			row.forEach(function(col) {
+				if(col.empty) { empty++; }
+			});
+		});
+		this.set('full', empty === 0);
+		this.set('empty', empty);
+	},
 	didInsertElement: function() {
 		this._super();
 
@@ -36,6 +48,21 @@ export default Ember.Component.extend(InboundActions, {
 	actions: {
 		addButton: function(button) {
 			console.log(button);
+			//this.get('suggestionController').send('add_item', button);
+			this.check_availability();
+			if(this.get('full')) { return; }
+			var board = this.get('model.board');
+			var spot = editManager.find_button('empty');
+			if(spot) {
+				editManager.change_button(spot.id, {
+					label: button.label,
+					sound: button.sound,
+					sound_id: button.sound_id,
+					external_id: button.external_id
+				});
+			} else {
+				editManager.lucky_symbol(spot.id);
+			}
 		}
 	}
 });
