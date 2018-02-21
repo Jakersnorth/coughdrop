a949553f7142807c4c366566735e23c94728f308
John Akers <johnfakersnorth@gmail.com>
2018-02-20 20:04:44 -0800

    changes suggestion sidebar display to only show suggested labels and moves add to board functionality to this branch

--- app/frontend/app/components/suggested-buttons.js
+++ app/frontend/app/components/suggested-buttons.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import InboundActions from '../../ember-component-inbound-actions/inbound-actions';
+import editManager from '../utils/edit_manager';
 
 export default Ember.Component.extend(InboundActions, {
 	slideoutService: Ember.inject.service('slideout-service'),
@@ -7,7 +8,17 @@ export default Ember.Component.extend(InboundActions, {
 		this._super();
 		this.set('slideout', null);
 	},
-
+	check_availability: function() {
+		var rows = editManager.controller.get('ordered_buttons');
+		var empty = 0;
+		rows.forEach(function(row) {
+			row.forEach(function(col) {
+				if(col.empty) { empty++; }
+			});
+		});
+		this.set('full', empty === 0);
+		this.set('empty', empty);
+	},
 	didInsertElement: function() {
 		this._super();
 
@@ -36,6 +47,20 @@ export default Ember.Component.extend(InboundActions, {
 	actions: {
 		addButton: function(button) {
 			console.log(button);
+			this.check_availability();
+			if(this.get('full')) { return; }
+			var board = this.get('model.board');
+			var spot = editManager.find_button('empty');
+			if(spot) {
+				editManager.change_button(spot.id, {
+					label: button.label,
+					sound: button.sound,
+					sound_id: button.sound_id,
+					external_id: button.external_id
+				});
+			} else {
+				editManager.lucky_symbol(spot.id);
+			}
 		}
 	}
 });
