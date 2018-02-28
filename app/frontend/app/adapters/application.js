e812823c908379ca9e1c5f1316823e5e1f96c8b5
John Akers <johnfakersnorth@gmail.com>
2018-02-28 12:18:30 -0800

    modifies styling of sidebar to dispaly suggestions in a button format, still need to add images

--- app/frontend/app/adapters/application.js
+++ app/frontend/app/adapters/application.js
@@ -3,7 +3,10 @@ import DS from "ember-data";
 import persistence from '../utils/persistence';
 
 var res = DS.RESTAdapter.extend({
-  namespace: 'api/v1'
+  namespace: 'api/v1',
+  headers: {
+  	'Access-Control-Allow-Origin': '*'
+  }
 }, persistence.DSExtend);
 
 export default res;
